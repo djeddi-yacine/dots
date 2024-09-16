96b23b91f30c6fbf9386626239ebe1dfbf5744b3
djeddi-yacine <yacine2700dj@gmail.com>
2024-09-16 20:05:42 +0100

    add sh scripts

---
+++ scripts/chromuim.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+chromium --use-vulkan --enable-features=Vulkan --disable-infobars  --disable-client-side-phishing-detectio --disable-features=InterestFeedContentSuggestions  --no-default-browser-check  --disable-prompt-on-repost --noerrdialogs --no-pings --disable-hang-monitor --disable-crash-reporter --enable-tcp-fast-open --enable-quic --no-crash-upload 
